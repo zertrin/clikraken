5e36696f1ecf133d92d7c733b1f9107d2cc21741
zertrin <zertrin@gmail.com>
2016-11-20 21:49:53 +0100

    Use pypandoc.convert_file() since pypandoc.convert() is now deprecated.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import clikraken
 # right place.
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = pypandoc.convert_file('README.md', 'rst')
 except (IOError, ImportError):
     long_description = open('README.md').read()
 
