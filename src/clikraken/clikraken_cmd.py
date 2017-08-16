60f4f2b194ec8054b5a6049f96f87d1ff4d60e06
Jaime Alberto Sanchez Hidalgo <jaime@grupocitec.com>
2017-08-16 18:35:54 -0300

    Add TradesHistory and QueryTrades call as trades command

--- src/clikraken/clikraken_cmd.py
+++ src/clikraken/clikraken_cmd.py
@@ -27,6 +27,7 @@ from clikraken.api.private.cancel_order import cancel_order
 from clikraken.api.private.get_balance import get_balance
 from clikraken.api.private.list_closed_orders import list_closed_orders
 from clikraken.api.private.list_open_orders import list_open_orders
+from clikraken.api.private.trades import trades
 from clikraken.api.private.place_order import place_order
 
 
@@ -185,6 +186,42 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser_clist.add_argument('-p', '--pair', default=None, help=pair_help)
     parser_clist.set_defaults(sub_func=list_closed_orders)
+
+    # List of closed orders
+    parser_trades = subparsers.add_parser(
+        'trades',
+        aliases=['tr'],
+        help='[private] Get trades history',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_trades.add_argument(
+        '-t',
+        '--type',
+        default=None,
+        help='type of trade (optional). Values: all|any position|closed position|closing position|no position')
+    # TODO: trades parameter
+    parser_trades.add_argument(
+        '-s',
+        '--start',
+        default=None,
+        help='starting uniz timestamp or trade tx id of results (optional. exclusive)')
+    parser_trades.add_argument(
+        '-e',
+        '--end',
+        default=None,
+        help='ending uniz timestamp or trade tx id of results (optional. exclusive)')
+    parser_trades.add_argument(
+        '-o',
+        '--ofs',
+        default=None,
+        help='result offset')
+    parser_trades.add_argument(
+        '-i',
+        '--id',
+        default=None,
+        help='comma delimited list of transaction ids to query info about (20 maximum)')
+    parser_trades.add_argument('-p', '--pair', default=None, help=pair_help)
+    # TODO: trades parameter
+    parser_trades.set_defaults(sub_func=trades)
 
     args = parser.parse_args()
 
