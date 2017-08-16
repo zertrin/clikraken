eba42b95dc38c036ccc96e80bb791c89f5153423
Jaime Alberto Sanchez Hidalgo <jaime@grupocitec.com>
2017-08-15 21:02:38 -0300

    Add method get_deposit_addresses for calling DepositAddresses API. zertrin/clikraken#12

---
+++ src/clikraken/api/private/get_deposit_addresses.py
@@ -0,0 +1,58 @@
+# -*- coding: utf8 -*-
+
+"""
+clikraken.api.private.get_deposit_addresses
+
+This module queries the DepositAddresses method of Kraken's API
+and outputs the results in a tabular format.
+
+Licensed under the Apache License, Version 2.0. See the LICENSE file.
+"""
+
+from collections import OrderedDict
+
+from clikraken.api.api_utils import query_api
+from clikraken.clikraken_utils import format_timestamp
+from clikraken.clikraken_utils import _tabulate as tabulate
+
+
+def get_deposit_addresses(args=None):
+    """Get deposit addresses."""
+
+    # Parameters to pass to the API
+    api_params = {
+        'asset': args.asset,
+        'method': args.method,
+    }
+    if args.new:
+        api_params['new'] = args.new
+
+    res = query_api('private', 'DepositAddresses', api_params, args)
+
+    if not res:
+        return
+
+    if args.one:
+        # Get preferably not used addresses
+        addresses = [a for a in res if a.get('new', False)] or res
+        print(addresses[0]['address'])
+    else:
+        addresses_list = []
+        for address in res:
+            # Initialize an OrderedDict to garantee the column order
+            # for later use with the tabulate function
+            asset_dict = OrderedDict()
+            # Remove leading Z or X from asset pair if it is of length 4
+            asset_dict['asset'] = args.asset[1:] if len(args.asset) == 4 and args.asset[0] in ['Z', 'X'] else args.asset
+            asset_dict['address'] = address['address']
+            asset_dict['new'] = address.get('new', False)
+            asset_dict['expiretm'] = int(address.get('expiretm', 0)) > 0 and format_timestamp(int(address.get('expiretm', 0))) or ''
+            addresses_list.append(asset_dict)
+
+        if not addresses_list:
+            return
+
+        # sort by expiretm
+        addresses_list = sorted(addresses_list, key=lambda odict: odict['expiretm'])
+
+        print(tabulate(addresses_list, headers="keys"))
--- src/clikraken/clikraken_cmd.py
+++ src/clikraken/clikraken_cmd.py
@@ -26,6 +26,7 @@ from clikraken.api.public.asset_pairs import asset_pairs
 from clikraken.api.private.cancel_order import cancel_order
 from clikraken.api.private.get_balance import get_balance
 from clikraken.api.private.get_deposit_methods import get_deposit_methods
+from clikraken.api.private.get_deposit_addresses import get_deposit_addresses
 from clikraken.api.private.list_closed_orders import list_closed_orders
 from clikraken.api.private.list_open_orders import list_open_orders
 from clikraken.api.private.place_order import place_order
@@ -197,6 +198,18 @@ def parse_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser_deposit_methods.add_argument('-a', '--asset', default=gv.DEFAULT_ASSET, help='asset being deposited')
     parser_deposit_methods.set_defaults(sub_func=get_deposit_methods)
+
+    # Deposit Addresses
+    parser_deposit_addresses = subparsers.add_parser(
+        'deposit_addresses',
+        aliases=[],
+        help='[private] Get deposit addresses',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser_deposit_addresses.add_argument('-a', '--asset', default=gv.DEFAULT_ASSET, help='asset being deposited')
+    parser_deposit_addresses.add_argument('-m', '--method', default=None, help='name of the deposit method')
+    parser_deposit_addresses.add_argument('-n', '--new', action='store_true', default=False, help="whether or not to generate a new address")
+    parser_deposit_addresses.add_argument('-1', '--one', action='store_true', default=False, help="return just one address")
+    parser_deposit_addresses.set_defaults(sub_func=get_deposit_addresses)
 
     args = parser.parse_args()
 
