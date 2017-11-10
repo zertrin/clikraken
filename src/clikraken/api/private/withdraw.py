342eb60b8310fa797169fa9c20cc11de5ff9e4e0
Jaime Alberto Sanchez Hidalgo <jaime@grupocitec.com>
2017-11-10 10:51:43 -0200

    Remove ununsed imports

--- src/clikraken/api/private/withdraw.py
+++ src/clikraken/api/private/withdraw.py
@@ -9,11 +9,7 @@ and outputs the results in a tabular format.
 Licensed under the Apache License, Version 2.0. See the LICENSE file.
 """
 
-from collections import OrderedDict
-
 from clikraken.api.api_utils import query_api
-from clikraken.clikraken_utils import format_timestamp
-from clikraken.clikraken_utils import _tabulate as tabulate
 
 
 def withdraw(args=None):
