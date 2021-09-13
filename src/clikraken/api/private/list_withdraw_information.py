2f6bb9a9e0cd322cc910f4c73b7ed33606729a75
Frederic Lepied <flepied@gmail.com>
2021-09-13 21:36:27 +0200

    add the list_withdraw_information sub-command

---
+++ src/clikraken/api/private/list_withdraw_information.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+"""
+clikraken.api.private.list_withdraw_information
+
+This module queries the WithdrawInfo method of Kraken's API
+and outputs the results in a tabular format.
+
+Licensed under the Apache License, Version 2.0. See the LICENSE file.
+"""
+
+import argparse
+
+import clikraken.global_vars as gv
+
+from clikraken.api.api_utils import query_api
+from clikraken.clikraken_utils import _tabulate as tabulate
+from clikraken.clikraken_utils import csv
+
+
+def list_withdraw_information(args):
+    """List withdrawals."""
+
+    # Parameters to pass to the API
+    api_params = {"asset": args.asset}
+
+    res = query_api("private", "WithdrawInfo", api_params, args)
+
+    if args.csv:
+        print(csv(res, headers="keys"))
+    else:
+        print(tabulate(res, headers="keys"))
+
+
+def init(subparsers):
+    parser_list_withdrawal = subparsers.add_parser(
+        "list_withdraw_information",
+        aliases=["lwi"],
+        help="[private] List withdraw information",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser_list_withdrawal.add_argument(
+        "-a", "--asset", default=gv.DEFAULT_ASSET, help="asset to list"
+    )
+    parser_list_withdrawal.set_defaults(sub_func=list_withdraw_information)
